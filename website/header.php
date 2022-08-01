28b0c2dd197a86a4f3a3af2e17224778d075d14d
iyankdesu <iyankdesu@gmail.com>
2022-08-01 17:45:35 +0800

    Add Website & Database files

---
+++ website/header.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="jquery/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
---
+++ website/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "header.php"; ?>
+	<title>Menu Utama</title>
+</head>
+<body>
+	<?php include "menu.php"; ?>
+	<div class="container-fluid" style="padding-top: 5%; text-align: center; font-family: 'Open Sans';">
+		<h1 style="font-weight: bold;">
+			SISTEM ABSENSI <br>
+			BERBASIS KARTU RFID
+		</h1>
+		<br><hr><br>
+		<h2 style="font-weight: bold;">Oleh Kelompok 1</h2>
+		<br>
+		<h3>Muhammad Fajrian Nur Ibrahim</h3>
+		<h3>Muhammad Iqbal</h3>
+		<h3>Muhammad Noor Fauzan</h3>
+		<h3>Muhammad Rio Ramadhani</h3>
+		<h3>Muhammad Rully Adha</h3>
+		<h3>Najih Mahmudi</h3>
+		<h3>Rifky Alfarez</h3>
+	</div>
+</body>
+</html>
